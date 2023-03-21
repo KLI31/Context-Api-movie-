f14d7b775f5a9b6366268affbc520b2aaa433f6a
KLI31 <66835521+KLI31@users.noreply.github.com>
2023-03-21 13:06:58 -0500

    2 files modified, 1 added

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './componentStyle.css';
 import Movie from './Movie';
+import initialMovies from './consts/initialMovies';
 
 const MovieList = () => {
   return (
-    <div>
-      <Movie />
+    <div class="container">
+      <div class="row">
+        {initialMovies.map((movie) => (
+          <div class="columns" key={movie}>
+            <Movie movie={movie} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
